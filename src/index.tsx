0aca86f8e3009f1937ce31d35d1a6c09e9f7e765
KrysDev <webdevkrys@gmail.com>
2023-06-24 21:33:25 +0100

    first commit

---
+++ src/index.tsx
@@ -0,0 +1,21 @@
+import './index.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+
+function App() {
+  return (
+    <div>
+      XxXOX
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
